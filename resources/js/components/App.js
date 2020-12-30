2450ea9d2a0dc3714bc7d1d6f8879e539aa847b0
YUKI YAMAZAKI <gooo0523@gmail.com>
2020-12-30 17:46:01 +0900

    土台修正完成

---
+++ resources/js/components/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {BrowserRouter,Route,Switch} from 'react-router-dom';
+
+import GlobalNav from './GlobalNav';
+import Top from './Top';
+import About from './About';
+
+const App = () => {
+  return(
+    <BrowserRouter>
+    <React.Fragment>
+      <GlobalNav />
+      <Switch>
+        <Route path="/" exact component={Top} />
+        <Route path="/about" exact component={About} />
+      </Switch>
+    </React.Fragment>
+    </BrowserRouter>
+  )
+}
+
+if(document.getElementById('app')){
+  ReactDOM.render(<App />, document.getElementById('app'));
+}
